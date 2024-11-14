b31485f73e46416e42245db887ebbcb5d9b8e23f
Julien Vignoud <julien.vignoud@epfl.ch>
2024-11-14 15:33:11 +0100

    discojs/src/default_tasks: change default batch size and block size

--- discojs/src/default_tasks/wikitext.ts
+++ discojs/src/default_tasks/wikitext.ts
@@ -33,9 +33,9 @@ export const wikitext: TaskProvider<'text'> = {
         // But if set to 0 then the webapp doesn't display the validation metrics
         validationSplit: 0.1, 
         roundDuration: 2,
-        batchSize: 1, // If set too high (e.g. 16) firefox raises a WebGL error
+        batchSize: 8, // If set too high firefox raises a WebGL error
         tokenizer: 'Xenova/gpt2',
-        maxSequenceLength: 128,
+        maxSequenceLength: 64,
         tensorBackend: 'gpt'
       }
     }
